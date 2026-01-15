ce99084af49cfe847b7954b816ad7e5419ac7acc
GonGarce <gonzalo.nom.es@gmail.com>
2026-01-15 13:16:50 +0100

    Add pagination to blog posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,18 +1,25 @@
 import { draftMode } from 'next/headers';
 import BlogItem from './blog-item'
 import { getBlogPosts } from '@/lib/contentful/api';
-import { BlogPost } from '@/types/contentful';
+import { BlogPostPage } from '@/types/contentful';
 import { pathname } from 'next-extra/pathname';
 import { Metadata } from 'next';
 import { getPageMetadata } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import Link from "next/link"
 
 export async function generateMetadata(): Promise<Metadata> {
   return getPageMetadata(pathname());
 }
+const pageSize = 6;
 
-export default async function BlogPage() {
+export default async function BlogPage({ searchParams }: any) {
   const { isEnabled } = draftMode();
-  const posts = await getBlogPosts(4, isEnabled) as BlogPost[];
+  const page = Number(searchParams?.page ?? 1);
+  const skip = (page - 1) * pageSize;
+  const data = await getBlogPosts(pageSize, skip,isEnabled ) as BlogPostPage;
+
+  const totalPages = Math.ceil(data.total/pageSize);
 
   return (
     <section className="w-full py-12 md:py-24 bg-background">
@@ -27,7 +34,7 @@ export default async function BlogPage() {
           </div>
         </div>
         <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
-          {posts?.length > 0 && posts.map((post) => (
+          {data.posts.length > 0 && data.posts.map((post) => (
             <BlogItem
               key={post.sys.id}
               title={post.title}
@@ -36,6 +43,25 @@ export default async function BlogPage() {
               description={post.summary}
               href={`/blog/${post.slug}`} />
           ))}
+        </div>
+        <div className="mx-auto grid max-w-5xl items-center grid-cols-2 gap-6 lg:gap-12">
+          {page > 1 && 
+            <Link
+              href={pathname() + (page > 2 ? `/?page=${page - 1}` : '')}
+              prefetch={false}
+            >
+              <Button variant="outline" className="w-full" >&lt; Novas</Button>
+	    </Link>
+          }
+          {page < totalPages && 
+            <Link
+              className="w-full col-start-2" 
+              href={`${pathname()}/?page=${page + 1}`}
+              prefetch={false}
+            >
+              <Button variant="outline" className="w-full" >Antigas &gt;</Button>
+	    </Link>
+          }
         </div>
       </div>
     </section>
